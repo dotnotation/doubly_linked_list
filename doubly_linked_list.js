5c7a3e67733929cdf6c5a5c519f0ea52e7e100d6
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-16 17:20:27 -0500

    first attempt at solution for unshift

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -54,6 +54,7 @@ class DoublyLinkedList{
     }
 
     shift(){
+        // remove a node from the beginning 
         if (!this.head) return undefined
         
         let oldHead = this.head
@@ -68,5 +69,21 @@ class DoublyLinkedList{
         }
         this.length--
         return oldHead
+    }
+
+    unshift(val){
+        // add node to the beginning of the linked list
+        let newNode = new Node(val)
+
+        if (this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head 
+            this.head = newNode
+        }
+        this.length++ 
+        return this
     }
 }
