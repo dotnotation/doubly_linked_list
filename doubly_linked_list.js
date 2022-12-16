2440e805ecb11865f8d9e6e1619909decca45572
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-16 16:40:43 -0500

    first attempt at solution for push

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -12,4 +12,27 @@ class DoublyLinkedList{
         this.tail = null
         this.length = 0
     }
+
+    push(val){
+        // add a node to the end of the list
+        // create a new node with the val
+        // if head is null then head and tail are new node
+        // take current tail and set next to be new node
+        // take then set .prev to the old tail
+        // set tail to be new node
+
+        let newNode = new Node(val)
+
+        if (!head){
+            this.head = newNode
+            this.tail = newNode
+        }
+
+        let oldTail = this.tail
+        oldTail.next = newNode 
+        newNode.prev = oldTail 
+        this.tail = newNode 
+        this.length++
+        return this
+    }
 }
